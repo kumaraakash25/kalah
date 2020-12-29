38509efff345131fc88f35e26baa704b830dd2e5
aakashkumar <aakashthesid@gmail.com>
2020-12-30 00:42:59 +0200

    Removing not required Serialization and adding gitignore file

--- src/main/java/com/game/kalah/controller/GameController.java
+++ src/main/java/com/game/kalah/controller/GameController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
